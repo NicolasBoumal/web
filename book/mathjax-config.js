461a880bcf2cc78bb39c8288f4aec62d42a29005
Nicolas Boumal <nicolasboumal@gmail.com>
2023-07-18 16:43:34 +0200

    First attempt toward formatting exercises for the web

---
+++ book/mathjax-config.js
@@ -0,0 +1,23 @@
+window.MathJax = {
+    tex: {
+      inlineMath: [['$', '$'], ['\\(', '\\)']],
+      macros: {
+        reals: "\\mathbb{R}",
+        inner: ["\\left\\langle #1, #2 \\right\\rangle", 2],
+        smallinner: ["\\langle #1, #2 \\rangle", 2],
+        calM: "\\mathcal{M}",
+        SOd: "\\mathrm{SO}(d)",
+        T: "\\mathrm{T}",
+        skeww: "\\mathrm{skew}",
+        symm: "\\mathrm{sym}",
+        Proj: "\\mathrm{Proj}",
+        grad: "\\mathrm{grad}",
+        Hess: "\\mathrm{Hess}",
+        Retr: "\\mathrm{R}"
+      }
+    },
+    svg: {
+      fontCache: 'global'
+    }
+  };
+  
