56bff35dd2a315f3070a37f52b1acec2266ac5e3
Nicolas Boumal <nicolasboumal@gmail.com>
2023-09-05 16:31:32 +0200

    Updated by Tim

--- book/mathjax-config.js
+++ book/mathjax-config.js
@@ -6,20 +6,43 @@ window.MathJax = {
         inner: ["\\left\\langle #1, #2 \\right\\rangle", 2],
         smallinner: ["\\langle #1, #2 \\rangle", 2],
         calM: "\\mathcal{M}",
+        calN: "\\mathcal{N}",
         SOd: "\\mathrm{SO}(d)",
         T: "\\mathrm{T}",
         skeww: "\\mathrm{skew}",
-        symm: "\\mathrm{sym}",
+        symm: "\\mathrm{Sym}",
         Proj: "\\mathrm{Proj}",
         grad: "\\mathrm{grad}",
         Hess: "\\mathrm{Hess}",
         Retr: "\\mathrm{R}",
         D: "\\mathrm{D}",
-        Sd: "\\mathbb{S}^{d-1}"
+        Sd: "\\mathbb{S}^{d-1}",
+        Rd: "\\mathbb{R}^{d}",
+        calE: "\\mathcal{E}",
+        Stnp: "\\mathrm{St}(n,p)",
+        Rnp: "\\mathbb{R}^{n \\times p}",
+        Rnn: "\\mathbb{R}^{n \\times n}",
+        Rn: "\\mathbb{R}^{n}",
+        Sn: "\\mathbb{S}^{n}",
+        Sm: "\\mathbb{S}^{m}",
+        trace: "\\mathrm{Tr}",
+        Trace: "\\mathrm{Tr}",
+        diag: "\\mathrm{diag}",
+        SOn: "\\mathrm{SO}(n)",
+        rank: "\\mathrm{rank}",
+        frakX: "\\mathfrak{X}",
+        frakF: "\\mathfrak{F}",
+        One: "\\mathbf{1}",
+        dist: "\\mathrm{dist}",
+        calP: "\\mathcal{P}",
+        Rdd: "\\mathbb{R}^{d \\times d}",
+        calU: "\\mathcal{U}",
+        calV: "\\mathcal{V}",
+        calW: "\\mathcal{W}",
+        bigger: "<"
       }
     },
     svg: {
       fontCache: 'global'
     }
   };
-  
